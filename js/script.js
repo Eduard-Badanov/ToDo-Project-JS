5d8270f2fc47a34b084890a549adf9490018772d
Eduardik <badanov.edward@gmail.com>
2024-07-03 10:17:52 +0300

    add animation in complete-task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,2 @@
 if (document.querySelector('.task-left__text').clientWidth > 300) document.querySelector('.tasks-list__task').classList.add('animated')
+if (document.querySelector('.completed-task__text').clientWidth > 400) document.querySelector('.completed-tasks-list__completed-task').classList.add('animated')
