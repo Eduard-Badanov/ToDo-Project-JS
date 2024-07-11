3dc71e5769c632d7c00668531b8c97651d31edaf
Eduardik <badanov.edward@gmail.com>
2024-07-11 11:42:20 +0300

    Update deleteElements.js

--- js/listeners/deleteElements.js
+++ js/listeners/deleteElements.js
@@ -3,15 +3,19 @@ import { user } from '../userInfo.js'
 document.querySelector('.tasks__delete-button').addEventListener('click', () => {
 	while (document.querySelector('.tasks__tasks-list').childNodes.length > 0) {
 		document.querySelector('.tasks__tasks-list').childNodes.forEach((element) => {
+			const DOMname = element.classList[1].slice(0, -2)
+			const id = Number(element.classList[1].at(-1))
 			element.remove()
+			user.forEach((el) => {
+				if (el.DOMname === DOMname) {
+					el.tasks.forEach((element) => {
+						if (element.id === id) {
+							const indexOfTask = el.tasks.indexOf(element)
+							delete el.tasks[indexOfTask]
+						}
+					})
+				}
+			})
 		})
 	}
-	user.forEach((element) => {
-		if (element.tasks.length > 0) {
-			element.tasks.forEach((el) => {
-				const indexOfTask = element.tasks.indexOf(el)
-				delete element.tasks[indexOfTask]
-			})
-		}
-	})
 })
