edbb7bf7e554be010884bca2eabd65ebf80bfcaf
Eduardik <badanov.edward@gmail.com>
2024-07-20 09:39:53 +0300

    add production build of ToDo project

--- main.js
+++
@@ -1,1 +0,0 @@
-import './src/js/script.js'
--- src/js/addCompletedTaskItems/removeCompletedTaskItem.js
+++
@@ -1,7 +0,0 @@
-export function removeCompletedTaskItem() {
-	while (document.querySelector('.tasks__completed-tasks-list').childNodes.length > 0) {
-		document.querySelector('.tasks__completed-tasks-list').childNodes.forEach((element) => {
-			element.remove()
-		})
-	}
-}
