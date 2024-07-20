b3c6513ebdac2c9bf18b6ea5586934f62a0c5e26
Eduardik <badanov.edward@gmail.com>
2024-07-20 13:28:02 +0300

    fix paths to icons

--- src/js/pathsToIcons.js
+++ src/js/pathsToIcons.js
@@ -1,17 +1,17 @@
 export const scenarioIcons = {
-	bookmark: '/images/icons/scenario-icons/bookmark.svg',
-	briefcase: '/images/icons/scenario-icons/briefcase.svg',
-	delete: '/images/icons/scenario-icons/delete.svg',
-	home: '/images/icons/scenario-icons/home.svg',
-	info: '/images/icons/scenario-icons/info.svg',
-	plus: '/images/icons/scenario-icons/plus.svg',
-	sun: '/images/icons/scenario-icons/sun.svg',
-	user: '/images/icons/scenario-icons/user.svg'
+	bookmark: './images/icons/scenario-icons/bookmark.svg',
+	briefcase: './images/icons/scenario-icons/briefcase.svg',
+	delete: './images/icons/scenario-icons/delete.svg',
+	home: './images/icons/scenario-icons/home.svg',
+	info: './images/icons/scenario-icons/info.svg',
+	plus: './images/icons/scenario-icons/plus.svg',
+	sun: './images/icons/scenario-icons/sun.svg',
+	user: './images/icons/scenario-icons/user.svg'
 }
 export const taskIcons = {
-	circle: '/images/icons/task-icons/circle.svg',
-	circleActive: '/images/icons/task-icons/circle-active.svg',
-	delete: '/images/icons/task-icons/delete.svg',
-	star: '/images/icons/task-icons/star.svg',
-	starActive: '/images/icons/task-icons/star-active.svg'
+	circle: './images/icons/task-icons/circle.svg',
+	circleActive: './images/icons/task-icons/circle-active.svg',
+	delete: './images/icons/task-icons/delete.svg',
+	star: './images/icons/task-icons/star.svg',
+	starActive: './images/icons/task-icons/star-active.svg'
 }
