a25020a077e985069e992ef6668e50626ade49e6
Eduard-Badanov <badanov.edward@gmail.com>
2024-07-31 23:45:56 +0300

    fix imports

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 import { addItems } from './addScenarioItems/addItems.js'
 import { renderDefaultTasksOfActiveScenarioItem } from './addTaskItems/renderDefaultTasksOfActiveScenarioItem.js'
-import('./listeners/scenariosAdd.js')
-import('./listeners/scenarioItems.js')
-import('./listeners/sortingElements.js')
-import('./listeners/showCompletedTasks.js')
-import('./listeners/taskItems.js')
-import('./listeners/deleteElements.js')
+import './listeners/scenariosAdd.js'
+import './listeners/scenarioItems.js'
+import './listeners/sortingElements.js'
+import './listeners/showCompletedTasks.js'
+import './listeners/taskItems.js'
+import './listeners/deleteElements.js'
 
 addItems()
 
